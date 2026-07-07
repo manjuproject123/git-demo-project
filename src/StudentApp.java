e1e89091912bfb997a9d9bf992f1d829da9e31be
Musturu Manjunath <m.manjunath@aseuro.in>
2026-07-07 16:28:31 +0530

    Added email field to student

---
+++ src/StudentApp.java
@@ -0,0 +1,6 @@
+public class StudentApp {
+    public static void main(String[] args) {
+        Student student = new Student(101, "Manjunath", "Java Full Stack");
+        student.displayStudent();
+    }
+}
